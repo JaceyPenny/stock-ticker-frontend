175a539430b91090667ac7eb39c6c3bda201e0fa
Jace McPherson <jace.j.mcpherson@gmail.com>
2018-11-10 17:59:58 -0600

    Added "Remove stock" option

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, OnInit, AfterViewInit } from '@angular/core';
-import { Stock, StockService } from '../service/stock/stock.service';
+import { Stock, StockService, StockList } from '../service/stock/stock.service';
 import { SearchService, SearchResult } from '../service/search/search.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,18 +14,31 @@ export class DashboardComponent implements AfterViewInit {
     LOADING_SEARCH = 1;
     SEARCHING = 2;
 
-    stocks: Stock[] = []
+    stockList: StockList = new StockList();
     searchState = this.LOADING_SEARCH;
     searchResults: SearchResult[] = [];
 
-    constructor(private stockService: StockService, private searchService: SearchService) { }
+    get shouldShowSpinner(): boolean {
+        return this.searchState === this.LOADING_SEARCH || this.searchState === this.SEARCHING;
+    }
+
+    constructor(
+        private stockService: StockService,
+        private searchService: SearchService,
+        private snackBar: MatSnackBar) { }
+
+    showSnackBar(message: string, duration: number = 2000) {
+        this.snackBar.open(message, null, {
+            duration: duration
+        });
+    }
 
     ngAfterViewInit(): void {
         this.stockService.getStocks().then((response) => {
             if (typeof response === 'string') {
                 console.log(response);
             } else {
-                this.stocks = response;
+                this.stockList = response;
             }
         });
 
@@ -36,12 +50,23 @@ export class DashboardComponent implements AfterViewInit {
     }
 
     requestDelete(stock: Stock) {
-        console.log('Requested deletion of ' + stock.symbol);
+        this.stockList.remove(stock);
+        this.stockService.setStocks(this.stockList).then(ok => {
+            if (ok) {
+                this.showSnackBar('Removed ' + stock.symbol + ' from your stocks.');
+            } else {
+                this.showSnackBar('Something went wrong. ' + stock.symbol + ' was not added.');
+            }
+        });
     }
 
     requestSearch(value: string) {
         if (value.length <= 1) {
             this.searchResults = [];
+            return;
+        }
+
+        if (this.searchState === this.SEARCHING) {
             return;
         }
 
--- src/app/service/stock/stock.service.ts
+++ src/app/service/stock/stock.service.ts
@@ -9,6 +9,74 @@ export class Stock {
     symbol: string;
     price: number;
     close: number;
+
+    equals(other: Stock): boolean {
+        return other.name === this.name && other.symbol === this.symbol;
+    }
+
+    compareTo(other: Stock): number {
+        let otherName = other.name;
+        let thisName = this.name;
+
+        if (thisName == otherName) {
+            return 0;
+        } else if (thisName < otherName) {
+            return -1;
+        } else {
+            1;
+        }
+    }
+}
+
+export class StockList {
+    stocks: Stock[];
+
+    constructor(stocks: Stock[] = []) {
+        this.stocks = stocks;
+    }
+
+    size() {
+        return this.stocks.length;
+    }
+
+    sort() {
+        this.stocks.sort((a, b) => a.compareTo(b));
+    }
+
+    add(stock: Stock): boolean {
+        var desiredIndex = 0;
+        var counter = 0;
+        for (let s of this.stocks) {
+            let comparison = s.compareTo(stock);
+            if (comparison === -1) {
+                desiredIndex = counter;
+            } else if (comparison === 0) {
+                return false;
+            } else {
+                break;
+            }
+
+            counter += 1;
+        }
+
+        this.stocks.splice(desiredIndex, 0, stock);
+        return true;
+    }
+
+    remove(stock: Stock): boolean {
+        let stockIndex = this.stocks.findIndex(s => s.equals(stock));
+        if (stockIndex < 0) {
+            return false;
+        }
+
+        this.stocks.splice(stockIndex, 1);
+        return true;
+    }
+
+    toString(): string {
+        this.sort();
+        return this.stocks.map(s => s.symbol).join(',');
+    }
 }
 
 @Injectable()
@@ -40,7 +108,7 @@ export class StockService {
         });
     }
 
-    getStockQuotes = ((stockSymbols: string[]): Promise<Stock[]> => {
+    getStockQuotes = ((stockSymbols: string[]): Promise<StockList> => {
         return this.http.get(this.iexStocksUrl(stockSymbols)).toPromise().then(response => {
             stockSymbols.sort();
 
@@ -63,13 +131,28 @@ export class StockService {
                 }
             }
 
-            return stocks;
+            return new StockList(stocks);
         });
     }).bind(this);
 
-    getStocks(): Promise<Stock[]> {
+    getStocks(): Promise<StockList> {
         return this.getStockList().then((response) => {
             return this.getStockQuotes(response);
         });
+    }
+
+    setStocks(stockList: StockList): Promise<boolean> {
+        return this.http
+            .put(this.MASHAPE_URL, stockList.toString(), { headers: this.mashapeHeaders })
+            .toPromise()
+            .then(response => {
+                if (response && 'status' in response.json()) {
+                    return response.json()['status'] === 'ok'
+                } else {
+                    console.log('response was: ');
+                    console.log(response.json());
+                    return false;
+                }
+            });
     }
 }
